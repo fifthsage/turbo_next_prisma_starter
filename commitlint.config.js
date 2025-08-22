52777e24c532d1feb0b0e7e5f12b2075747e64a8
fifthsage <fifthsage@gmail.com>
2025-08-22 10:55:23 +0900

    chore(commitlint): commitlint 설정 파일 포맷 정리
    - 코드 포맷팅을 개선하여 가독성을 높임
    - 불필요한 공백 및 줄바꿈 제거

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 // commitlint.config.js
 
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0],
+  },
+};
